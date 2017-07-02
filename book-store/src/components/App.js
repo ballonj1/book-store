1ff76747e9cea180e78c1ececbf937963053a757
ballonj1 <ballonj1@tcnj.edu>
2017-07-01 20:39:19 -0700

    add routes

--- book-store/src/components/App.js
+++ book-store/src/components/App.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, IndexLink } from 'react-router';
+import {Link} from 'react-router';
 
-// This is a class-based component because the current
-// version of hot reloading won't hot reload a stateless
-// component at the top-level.
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <IndexLink to="/">Home</IndexLink>
-        {' | '}
-        <Link to="/fuel-savings">Demo App</Link>
-        {' | '}
-        <Link to="/about">About</Link>
-        <br/>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-App.propTypes = {
-  children: PropTypes.element
+const App = (props) => {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Scotch Books</a>
+          </div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/">About</Link></li>
+              <li><Link to="/">Book</Link></li>
+              <li><Link to="/">Cart</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {props.children}
+    </div>
+  );
 };
 
 export default App;
